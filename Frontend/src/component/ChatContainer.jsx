27402dfbe55a7e8a5640ef4810f1bcc442a76acc
Prakhar272006 <77096052+Prakhar272006@users.noreply.github.com>
2025-03-13 20:02:33 +0530

    fixed chat container bug

--- Frontend/src/component/ChatContainer.jsx
+++ Frontend/src/component/ChatContainer.jsx
@@ -23,7 +23,8 @@ const ChatContainer = () => {
     );
   }
   return (
-    <div className="flex-1 flex-col overflow-auto">
+    
+    <div className=" w-full flex flex-col overflow-auto">
       <ChatHeader />
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
         {messages.map((message) => (
@@ -52,7 +53,7 @@ const ChatContainer = () => {
       </div>
       <MessageInput />
     </div>
-  );
+      );
 };
 
 export default ChatContainer;
